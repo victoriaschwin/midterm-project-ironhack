70076d1e6a44af2066c3f37153578ee5869bfb96
victoriaschwin <vicky.schwint95@gmail.com>
2022-11-19 14:28:34 +0100

    js bug, css project & contact

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,15 @@
-import fetch from 'node-fetch';
+import fetch from '../node_modules/node-fetch';
 
-
-// fetch("http://makeup-api.herokuapp.com/api/v1/products.json")
-//   .then((res) => res.json())
-//   .then((data) => dataApi = data)
-//   .catch((err) => console.log(err));
-
-
-  
   const response = await fetch("http://makeup-api.herokuapp.com/api/v1/products.json?product_tags=vegan");
   const data = await response.json();
   
+  let newdata = data.slice(0,3)
   
-  
-  
+  console.log(newdata[0].name)
+  let img1 = document.getElementById('project1-img')
+
+  let projectName = document.getElementById('project1-h2')
+
+  projectName.innerHTML = newdata[0].name
+
+  console.log(projectName.innerHTML)
